7e2adb26bf339869565cb1d7c24cb16ba90ea469
yinjun <yinjun@shootsoft.net>
2015-07-24 22:38:19 +1000

    add sb2 admin

---
+++ src/api/services/weidian.js
@@ -0,0 +1,11 @@
+/**
+ * Weidian
+ *
+ * @description :: Server-side logic for managing weidians
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+

f191fade6dc7f6398a50f4760417a16c88a1598e
yinjun <yinjun@shootsoft.net>
2015-07-26 14:40:12 +1000

    update menu & complete cart

--- src/assets/js/modules/product/index.js
+++ src/assets/js/modules/product/index.js
@@ -1,94 +1,111 @@
-
 $(document).ready(function() {
-    $('#dataTables-example').DataTable({
-        "processing": true,
-        "serverSide": true,
-        "ajax": {
-            "url": "/product/query",
-            "type": "POST"
-        },
-        "columns": [
-            { "data": "thumb_imgs", "orderable": false },
-            { "data": "price" },
-            { "data": "item_desc" },
-            { "data": "createdAt", "orderable": false }
-        ],
-        //TODO: set last column order false
-        responsive: true,
-        "rowCallback": function( row, data, index ) {
-		 	
-        	$('td:eq(0)', row).html( '<img class="responsive" alt="" src="'+data.thumb_imgs+'" />');
-        	$('td:eq(1)', row).html(data.price+'￥');
-        	$('td:eq(3)', row).html('<button onclick="cart.add('+data.itemid+')" data-toggle="modal" data-target="#cart" class="btn btn-success">Order</button>')
-		 	//cart.products[data.id] = data
-		    // if (data.image){
-		    // 	//data.image = '<img style="height:150px;width:150px" src="'+data.image+'" />'
-		    // 	//$('td:eq(1)', row).html( '<img style="height:150px;width:150px" alt="" src="'+data.image+'" />');
-		    // } else {
-		    // 	data.image = '';
-		    // }
-
-		    // if (data.desc && data.desc.length > 400){
-		    // 	$('td:eq(3)', row).html( 
-		    // 		'<span class="desctip" data-toggle="tooltip" data-placement="top"  title="'
-		    // 		+data.desc+'">'+data.desc.substring(0, 400)+'...</span>');
-		    // }
-		    // $('td:eq(2)', row).html('$'+data.price);
-		    //$('td:eq(4)', row).html('<button onclick="cart.add('+data.id+')" data-toggle="modal" data-target="#cart" class="btn btn-success">Order</button>')
-		},
-
-		"drawCallback": function( settings ) {
-	        // tooltip demo
-		    $('.desctip').tooltip()
-
-	    }
-       
-    });
-    
-
-    $('#genorder').bind('click', function(){
-    	$('#orderform').submit()
+    $('#genorder').bind('click', function() {
+        $('#orderform').submit()
     })
 });
 
-// var CartUpdater = React.createClass({
-//     render: function() {
-//         return (
-//         	<div>
-// 				{
-// 					this.props.purchased.map(function(product) {
-// 						return <div class="row">
-// 							<div class="col-lg-5">{product.name}</div>
-// 							<div class="col-lg-6"></div>
-// 							<div class="col-lg-1">${product.price}</div>
-// 							<input type="hidden" value={product.id} name="product_id[]" />
-// 						</div>;
-// 					})
-// 				}
-// 			</div>
-// 		)
-        		
-//     }
-// });
+var app = angular.module('cartApp', ['ngCart'])
+app.controller('cartCtrl', function($scope) {
+    $scope.init = function() {
+    	$scope.db = {}
+        $scope.products = []
+        $scope.summary = 0
+        $scope.weight = 0
+        $scope.ship = 0
+        $scope.config = {
+        	per_kg : 35
+        }
+        
+        $scope.init_dataTable()
+    }
 
-// var cart ={
-// 	products:{},
-// 	purchased:[],
-// 	add:function(id){
-// 		var p = cart.products[id]
-// 		if(p){
-// 			cart.purchased.push(p)
-// 			var pu = cart.purchased;
-// 			React.render(<CartUpdater purchased={pu} />, document.getElementById('cartbody'));
-// 			// var ui = '<div class="row">'
-// 			// 		+ '<div class="col-lg-5">'+p.name+'</div>'
-// 			// 		+ '<div class="col-lg-6">'+p.image+'</div>'
-//    //      			+ '<div class="col-lg-1"></div>'
-//    //      			+ '<input type="hidden" value="'+p.id+'" name="product_id[]" /></div>'
-//    //      	$('#cartbody').append(ui)
+    $scope.init_dataTable = function() {
+        $('#dataTables-example').DataTable({
+            "processing": true,
+            "serverSide": true,
+            "ajax": {
+                "url": "/product/query",
+                "type": "POST"
+            },
+            "columns": [{
+                "data": "thumb_imgs",
+                "orderable": false
+            }, {
+                "data": "price"
+            }, {
+                "data": "item_desc"
+            }, {
+                "data": "createdAt",
+                "orderable": false
+            }],
+            //TODO: set last column order false
+            responsive: true,
+            "rowCallback": function(row, data, index) {
+            	$scope.db[data.itemid] = data
+                $('td:eq(0)', row).html('<img class="responsive" alt="" src="' + data.thumb_imgs + '" />')
+                //$('td:eq(1)', row).html(data.price)
+                $('td:eq(3)', row).html('<select class="form-control" id="quality' + data.itemid + '"><option>1</option><option>2</option><option>3</option><option>4</option><option>5</option><option>6</option><option>7</option><option>8</option><option>9</option><option>10</option></select>'
+                	+'<button onclick="hack_add(' + data.itemid + ')" data-toggle="modal" data-target="#cart" class="btn btn-success">Order</button>')
+            },
 
-			
-// 		}
-// 	}
+            "drawCallback": function(settings) {
+                // tooltip demo
+                //$('.desctip').tooltip()
 
-// }
+            }
+
+        });
+    }
+
+    $scope.add_product = function(itemid, quality){
+    	
+    	var found = false
+    	var price = 0
+    	quality = parseInt(quality)
+    	for (var i in $scope.products){
+    		if($scope.products[i].itemid == itemid){
+    			$scope.products[i].quality = parseInt($scope.products[i].quality) + quality
+    			found = true
+    			price = $scope.products[i].price
+    			break
+    		}
+    	}
+
+    	var d = $scope.db[itemid]
+
+    	if(!found && d!=undefined){
+    		$scope.products.push({
+    			itemid: d.itemid,
+    			item_name : d.item_name,
+    			quality : quality,
+    			price : d.price
+    		})
+    		price = d.price
+    	}
+    	$scope.summary += price * quality
+    	$scope.$apply();
+    }
+
+    $scope.summary_price = function(){
+    	
+    	var sum = 0;
+    	for (var i in $scope.products){
+    		sum += $scope.products[i].quality * $scope.products[i].price
+    	}
+    	$scope.summary = sum
+    }
+
+    //init current app.controller
+    $scope.init()
+
+});
+
+
+function hack_add(itemid){
+  	var scope = angular.element($("#capp")).scope()
+  	var quality = $('#quality'+itemid).val()
+  	if (quality == undefined){
+  		quality = 1
+  	}
+   	scope.add_product(itemid, parseInt(quality))
+}
